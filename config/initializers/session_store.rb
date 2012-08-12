44129063b4d3dd05bfba211cf9a727b1c965e02f
Marco Antonio Filho <marcoafilho@gmail.com>
2012-08-12 16:11:00 -0300

    Refs #1. Initial commit; Rails new app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WithThisSong::Application.config.session_store :cookie_store, key: '_with-this-song_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WithThisSong::Application.config.session_store :active_record_store
