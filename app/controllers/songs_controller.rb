e282abd485dbf051731297096b328450eb9c26ca
Marco Antonio Almeida <marco.almeida@karnovgroup.com>
2016-12-29 13:48:35 +0100

    Update songs views

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,17 +6,17 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
 
-    render json: @song.to_json( 
-                   include: { interpreters: { only: :name }, 
+    render json: @song.to_json(
+                   include: { interpreters: { only: :name },
                               genres: { only: :name } })
   end
 
   def new
     @song = Song.new
   end
-  
+
   def create
-    @song = Song.new(params[:song])
+    @song = Song.new(song_params)
     if @song.save
       flash[:success] = I18n.t('songs.create.success')
       redirect_to songs_path
@@ -28,19 +28,31 @@ class SongsController < ApplicationController
   def edit
     @song = Song.find(params[:id])
   end
-  
+
   def update
     @song = Song.find(params[:id])
-    if @song.update_attributes(params[:song])
+    if @song.update(song_params)
       flash[:success] = I18n.t('songs.update.success')
       redirect_to songs_path
     else
       render 'edit'
     end
   end
-  
+
   def destroy
     @song = Song.find(params[:id]).destroy
     redirect_to songs_path
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(
+      :title,
+      :album,
+      :rating,
+      interpreter_ids: [],
+      genre_ids: []
+    )
   end
 end
